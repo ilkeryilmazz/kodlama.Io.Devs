c49ce469abdcd4a197f6f2668fa339e593d0a9be
ilkeryilmazz <ilkerylmaz.y@gmail.com>
2022-09-08 18:55:55 +0300

    .

---
+++ src/kodlama.Io.Devs/Application/Features/Auths/Commands/RegisterCommand/RegisterCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using Core.Security.Dtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Auths.Commands.RegisterCommand
+{
+    public class RegisterCommandValidator:AbstractValidator<RegisterCommand>
+    {
+        public RegisterCommandValidator()
+        {
+            RuleFor(u=>u.UserForRegister.Email).NotEmpty();
+            RuleFor(u => u.UserForRegister.Email).EmailAddress();
+            RuleFor(u => u.UserForRegister.Password).NotEmpty();
+            RuleFor(u => u.UserForRegister.FirstName).NotEmpty();
+            RuleFor(u => u.UserForRegister.LastName).NotEmpty();
+        }
+    }
+}
---
+++ src/kodlama.Io.Devs/Application/Features/Auths/Queries/LoginQuery/LoginQueryValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Auths.Queries.LoginQuery
+{
+    public class LoginQueryValidator:AbstractValidator<LoginQuery>
+    {
+        public LoginQueryValidator()
+        {
+            RuleFor(u => u.UserForLoginDto.Email).NotEmpty();
+            RuleFor(u => u.UserForLoginDto.Email).EmailAddress();
+            RuleFor(u => u.UserForLoginDto.Password).NotEmpty();
+        }
+    }
+}
